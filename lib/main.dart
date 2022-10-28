f57f7537a89529768bbd695a001ac51b1fa2301a
JuHyerin <jhr67370719@gmail.com>
2022-10-28 18:26:23 +0900

    clean

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_state_manager/blocs/counter/counter_bloc.dart';
 import 'package:flutter_state_manager/models/cart.dart';
 import 'package:flutter_state_manager/repositories/item_list.dart';
+import 'package:flutter_state_manager/screens/bloc_practice_screen/counter_screen.dart';
+import 'package:flutter_state_manager/screens/bloc_practice_screen/home_screen.dart';
 import 'package:flutter_state_manager/screens/provider_practice_screen/cart_screen.dart';
 import 'package:flutter_state_manager/screens/provider_practice_screen/item_screen.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
-  runApp(const MyApp());
+  runApp(const ProviderApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ProviderApp extends StatelessWidget {
+  const ProviderApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -35,7 +38,15 @@ class MyApp extends StatelessWidget {
         );
       },
     );
-    /*
+
+  }
+}
+
+class BlocApp extends StatelessWidget {
+  const BlocApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return BlocProvider( // BloC 을 사용할 위젯을 감쌈
       create: (context) => CounterBloc(), // 전역 Bloc 생성
       child: MaterialApp(
@@ -51,6 +62,5 @@ class MyApp extends StatelessWidget {
         initialRoute: '/',
       ),
     );
-    */
   }
 }
