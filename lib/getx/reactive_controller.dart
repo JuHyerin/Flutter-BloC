02bb86deef4efc8ab2a8deeeceda40fc46375269
JuHyerin <jhr67370719@gmail.com>
2022-10-31 17:33:40 +0900

    GetX Rx Worker test

--- lib/getx/reactive_controller.dart
+++ lib/getx/reactive_controller.dart
@@ -8,4 +8,24 @@ class ReactiveController extends GetxController {
   void increase() {
     counter++; // Observable 변수이므로 직접 update() 하여 알리지 않아도 됨
   }
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    // Rx 의 Worker 등록
+    once(counter, (_) => print('[once] $counter가 처음 변경되었습니다.'));
+    ever(counter, (_) => print('[ever] $counter가 변경되었습니다.'));
+    debounce(
+      counter,
+      (_) => print('[debounce] $counter가 마지막 변경이후 , 5초간 변경이 없습니다.'),
+      time: const Duration(seconds: 5)
+    );
+    interval(
+      counter,
+      (_) => print('[debounce] $counter가 변경되는 중입니다. (5초마다 호출)'),
+      time: const Duration(seconds: 5)
+    );
+  }
+
 }
